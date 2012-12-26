16e086de387d28a15d73806ab60289e0c5e5e2a7
Andreas Simon <a.simon@one-os.de>
2012-12-26 21:53:54 +0100

    More black voodoo to simplify the test framework

--- src/main/groovy/CommandRouter.groovy
+++ src/main/groovy/CommandRouter.groovy
@@ -1,19 +1,12 @@
-import commands.Lock_out_device
-import commands.Lock_out_device_Handler
-import commands.Register_new_device_Handler
-
 class CommandRouter {
     def repository
     def eventPublisher
 
     void route(command) {
-        def handler
+        handlerFor(command).handle(command)
+    }
 
-        if (command instanceof Lock_out_device) {
-            handler = new Lock_out_device_Handler(repository, eventPublisher)
-        } else {
-            handler = new Register_new_device_Handler(repository, eventPublisher)
-        }
-        handler.handle(command)
+    private def handlerFor(command) {
+        Class.forName("${command.class.name}_Handler").newInstance(repository, eventPublisher)
     }
 }
