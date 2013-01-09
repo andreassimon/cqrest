821c472e4390b2e0255312e2d5630a5b674fda85
Andreas Simon <a.simon@one-os.de>
2013-01-09 13:05:10 +0100

    Extracted test utility class IntegrationTest

--- src/test/groovy/integration/DeviceIntegrationTest.groovy
+++ src/test/groovy/integration/DeviceIntegrationTest.groovy
@@ -1,65 +1,26 @@
 package integration
 
-import com.rabbitmq.client.ConnectionFactory
-import domain.commands.*
-import infrastructure.Repository
-import infrastructure.messaging.AMQPEventPublisher
+import domain.commands.Register_new_device
 import org.junit.*
-import org.postgresql.ds.PGSimpleDataSource
-import org.springframework.jdbc.core.JdbcTemplate
-import readmodels.*
-import readmodels.eventhandlers.*
-import utilities.InMemoryRepository
-
-import javax.sql.DataSource
+import readmodels.DeviceSummary
 
 import static org.hamcrest.Matchers.equalTo
 import static org.junit.Assert.assertThat
 
-class DeviceIntegrationTest {
+class DeviceIntegrationTest extends IntegrationTest {
 
-    CommandRouter commandRouter
-    ReadModelRepository readModelRepository
-    ReadModelBuilder readModelBuilder
 
     @Before
     public void setUp() throws Exception {
-        def factory = new ConnectionFactory()
-        factory.virtualHost = 'one-os-test'
-        def connection = factory.newConnection()
-
-        def amqpEventPublisher = new AMQPEventPublisher(connection)
-        Repository repository = new InMemoryRepository()
-
-        commandRouter = new CommandRouter()
-        commandRouter.eventPublisher = amqpEventPublisher
-        commandRouter.repository     = repository
-
-        DataSource dataSource = new PGSimpleDataSource()
-        dataSource.user = 'user12'
-        dataSource.password = '34klq*'
-        dataSource.databaseName = 'one-os-cqrs'
-
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource)
-
-        jdbcTemplate.execute('DROP TABLE IF EXISTS DeviceSummary')
-        jdbcTemplate.execute(
-            'CREATE TABLE IF NOT EXISTS DeviceSummary (' +
-            '    deviceId uuid PRIMARY KEY,' +
-            '    deviceName VARCHAR(25)' +
-            ');'
-        )
-
-        readModelRepository = new ReadModelRepository(jdbcTemplate)
-        readModelBuilder = new ReadModelBuilder(connection)
-        readModelBuilder.eventHandlers = [
-                new New_user_was_created_Handler(),
-                new New_device_was_registered_Handler(),
-                new Device_was_locked_out_Handler(),
-                new Device_was_unregistered_Handler()
-        ]
-        readModelBuilder.jdbcTemplate = jdbcTemplate
+        dropTables(jdbcTemplate)
+        createTables(jdbcTemplate)
         readModelBuilder.start()
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dropTables(jdbcTemplate)
+        readModelBuilder.interrupt()
     }
 
     @Test
@@ -70,14 +31,8 @@ class DeviceIntegrationTest {
 
         Thread.sleep(100)
 
-        final allDeviceSummaries = readModelRepository.getAll(DeviceSummary.class)
+        final allDeviceSummaries = readModelRepository.getAll(DeviceSummary)
         assertThat allDeviceSummaries, equalTo([new DeviceSummary(deviceId, deviceName)])
     }
 
-
-
-    @After
-    public void tearDown() throws Exception {
-        readModelBuilder.interrupt()
-    }
 }
---
+++ src/test/groovy/integration/IntegrationTest.groovy
@@ -0,0 +1,111 @@
+package integration
+
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.impl.AMQConnection
+import domain.commands.CommandRouter
+import infrastructure.*
+import infrastructure.messaging.AMQPEventPublisher
+import org.postgresql.ds.PGSimpleDataSource
+import org.springframework.jdbc.core.JdbcTemplate
+import readmodels.*
+import readmodels.eventhandlers.*
+
+import javax.sql.DataSource
+
+abstract class IntegrationTest {
+
+    private AMQConnection amqpConnection
+    private DataSource dataSource
+    private JdbcTemplate jdbcTemplate
+    private Repository repository
+    private CommandRouter commandRouter
+    private ReadModelBuilder readModelBuilder
+    private ReadModelRepository readModelRepository
+
+
+
+    ReadModelBuilder getReadModelBuilder() {
+        if (readModelBuilder) { return readModelBuilder }
+
+        readModelBuilder = new ReadModelBuilder(getAmqpConnection())
+        readModelBuilder.eventHandlers = [
+            new New_user_was_created_Handler(),
+            new New_device_was_registered_Handler(),
+            new Device_was_locked_out_Handler(),
+            new Device_was_unregistered_Handler()
+        ]
+        readModelBuilder.jdbcTemplate = getJdbcTemplate()
+        return readModelBuilder
+    }
+
+    ReadModelRepository getReadModelRepository() {
+        if (readModelRepository) { return readModelRepository }
+
+        readModelRepository = new ReadModelRepository(jdbcTemplate)
+        readModelRepository
+    }
+
+    CommandRouter getCommandRouter() {
+        if(commandRouter) { return commandRouter }
+        commandRouter = new CommandRouter()
+        commandRouter.eventPublisher = new AMQPEventPublisher(getAmqpConnection())
+        commandRouter.repository = getRepository()
+        commandRouter
+    }
+
+    AMQConnection getAmqpConnection() {
+        if (amqpConnection) {
+            return amqpConnection
+        }
+        def amqpConnectionFactory = new ConnectionFactory(virtualHost: 'one-os-test')
+        amqpConnection = amqpConnectionFactory.newConnection()
+        return amqpConnection
+    }
+
+    DataSource getDataSource() {
+        if (dataSource) { return dataSource }
+
+        dataSource = new PGSimpleDataSource()
+        dataSource.user = 'user12'
+        dataSource.password = '34klq*'
+        dataSource.databaseName = 'one-os-cqrs'
+        dataSource
+    }
+
+    JdbcTemplate getJdbcTemplate() {
+        if (jdbcTemplate) { return jdbcTemplate }
+
+        jdbcTemplate = new JdbcTemplate(getDataSource())
+        jdbcTemplate
+    }
+
+    Repository getRepository() {
+        if (repository) { return repository }
+        repository = new JdbcRepository(jdbcTemplate: getJdbcTemplate())
+        repository
+    }
+
+    void createTables(JdbcTemplate jdbcTemplate) {
+        jdbcTemplate.execute(
+            'CREATE TABLE Events (' +
+            '    AggregateClassName varchar(255) NOT NULL,' +
+            '    AggregateId uuid NOT NULL,' +
+            '    EventName varchar(255) NOT NULL,' +
+            '    Attributes text NOT NULL,' +
+            '    Timestamp timestamp NOT NULL' +
+            ');'
+        )
+        jdbcTemplate.execute(
+            'CREATE TABLE DeviceSummary (' +
+            '    deviceId uuid PRIMARY KEY,' +
+            '    deviceName VARCHAR(25)' +
+            ');'
+        )
+    }
+
+    void dropTables(JdbcTemplate jdbcTemplate) {
+        jdbcTemplate.execute('DROP TABLE IF EXISTS Events;')
+        jdbcTemplate.execute('DROP TABLE IF EXISTS DeviceSummary;')
+    }
+
+}
