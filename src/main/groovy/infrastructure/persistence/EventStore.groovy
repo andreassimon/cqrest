2d425be822f7a473ad4df31c92310199f13f6d01
Andreas Simon <a.simon@one-os.de>
2013-02-13 15:54:11 +0100

    Added thrown RuntimeExceptions to the EventStore interface

--- src/main/groovy/infrastructure/persistence/EventStore.groovy
+++ src/main/groovy/infrastructure/persistence/EventStore.groovy
@@ -5,7 +5,7 @@ import domain.events.EventEnvelope
 public interface EventStore {
 
     def save(EventEnvelope eventEnvelope)
-    def getAggregate(String applicationName, String boundedContextName, String aggregateName, Class aggregateClass, UUID aggregateId, String eventPackageName)
-    def getEventsFor(String applicationName, String boundedContextName, String aggregateName, UUID aggregateId, String eventPackageName)
+    def getAggregate(String applicationName, String boundedContextName, String aggregateName, Class aggregateClass, UUID aggregateId, String eventPackageName) throws UnknownAggregate
+    def getEventsFor(String applicationName, String boundedContextName, String aggregateName, UUID aggregateId, String eventPackageName) throws UnknownAggregate
 
 }
