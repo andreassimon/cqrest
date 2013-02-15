000ebf296f9332deabc1cc8dce08e2c95ef32d13
Andreas Simon <a.simon@one-os.de>
2013-02-15 09:58:03 +0100

    Test refactoring

--- src/main/groovy/de/oneos/cqrs/readmodels/Projection.groovy
+++ src/main/groovy/de/oneos/cqrs/readmodels/Projection.groovy
@@ -5,4 +5,14 @@ class Projection {
     Map eventFilter
     Closure function
 
+    @Override
+    String toString() {
+        "Projection<$eventFilter, ${function.toString()}>"
+    }
+
+    @Override
+    boolean equals(Object that) {
+        this.eventFilter == that.eventFilter &&
+        this.function == that.function
+    }
 }
--- src/test/groovy/de/oneos/cqrs/readmodels/ProjectionBuilderTest.groovy
+++ src/test/groovy/de/oneos/cqrs/readmodels/ProjectionBuilderTest.groovy
@@ -36,38 +36,12 @@ class ProjectionBuilderTest {
 
         List<Projection> actualProjections = projectionBuilder.buildFrom projections
 
-        assertThat actualProjections[0].eventFilter, equalTo(eventFilterA)
-        assertThat actualProjections[0].function, equalTo(projectionFunctionA)
-
-        assertThat actualProjections[1].eventFilter, equalTo(eventFilterB)
-        assertThat actualProjections[1].function, equalTo(projectionFunctionB)
+        assertThat actualProjections, equalTo([
+            new Projection(eventFilter: eventFilterA, function: projectionFunctionA),
+            new Projection(eventFilter: eventFilterB, function: projectionFunctionB)
+        ])
     }
 
-    @Ignore
-    @Test
-    void should_build_update_projection() {
-        def projections = {
-            project(eventName: 'Device was locked out') { models, event ->
-                models.findAll { model ->
-                    model.deviceId == UUID.fromString(event.aggregateId)
-                }.each { model ->
-                    model.locked = true
-                }
-            }
-        }
-    }
-
-    @Ignore
-    @Test
-    void should_build_delete_projection() {
-        def projections = {
-            project(eventName: 'Device was deregistered') { models, event ->
-                models.findAll { model ->
-                    model.deviceId == UUID.fromString(event.aggregateId)
-                }.delete()
-            }
-        }
-    }
 
     static class DeviceDetails {
 
