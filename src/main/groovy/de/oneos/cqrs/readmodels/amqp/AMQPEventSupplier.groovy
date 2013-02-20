2d83aa502819fd265eef3f5d43f36bcbf48a6186
Andreas Simon <a.simon@one-os.de>
2013-02-20 10:31:07 +0100

    Plugged AMQPEventSupplier into AMQP

--- src/main/groovy/de/oneos/cqrs/readmodels/amqp/AMQPEventSupplier.groovy
+++ src/main/groovy/de/oneos/cqrs/readmodels/amqp/AMQPEventSupplier.groovy
@@ -1,13 +1,30 @@
 package de.oneos.cqrs.readmodels.amqp
 
-import com.rabbitmq.client.Channel
 import de.oneos.cqrs.readmodels.EventFilter
 import de.oneos.cqrs.readmodels.EventProcessor
 import de.oneos.cqrs.readmodels.EventSupplier
+import com.rabbitmq.client.*
+import groovy.json.JsonSlurper
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
-class AMQPEventSupplier implements EventSupplier {
+class AMQPEventSupplier extends DefaultConsumer implements Consumer, EventSupplier {
+    static Log log = LogFactory.getLog(AMQPEventSupplier)
+
+    JsonSlurper slurper = new JsonSlurper()
+
     Channel channel
     String queueName
+    List<EventProcessor> eventProcessors = []
+
+    AMQPEventSupplier(Connection connection) {
+        this(connection.createChannel())
+    }
+
+    AMQPEventSupplier(Channel channel) {
+        super(channel)
+        setChannel(channel)
+    }
 
     static String routingKey(EventFilter eventFilter) {
         eventFilter.withConstrainedValues(['applicationName', 'boundedContextName', 'aggregateName', 'eventName']) { constrainedValues ->
@@ -23,10 +40,26 @@ class AMQPEventSupplier implements EventSupplier {
     void declareQueue(Channel channel) {
         def declareOk = channel.queueDeclare()
         this.queueName = declareOk.queue
+        channel.basicConsume(queueName, AMQPConstants.NO_AUTO_ACK, this)
     }
 
     @Override
     void subscribeTo(EventFilter eventFilter, EventProcessor eventProcessor) {
         channel.queueBind(this.queueName, AMQPConstants.EVENT_EXCHANGE_NAME, routingKey(eventFilter))
+        eventProcessors << eventProcessor
+    }
+
+    @Override
+    void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+        Map deserializedEvent = slurper.parseText(new String(body))
+
+        try {
+            eventProcessors.each {
+                it.process(deserializedEvent)
+            }
+        } catch (Exception e) {
+            log.info "Exception was raised when handling event '${deserializedEvent.eventName}' ${deserializedEvent.attributes}", e
+        }
+        channel.basicAck(envelope.deliveryTag, AMQPConstants.SINGLE_MESSAGE)
     }
 }
--- src/test/groovy/de/oneos/cqrs/readmodels/amqp/AMQPEventSupplierTest.groovy
+++ src/test/groovy/de/oneos/cqrs/readmodels/amqp/AMQPEventSupplierTest.groovy
@@ -2,38 +2,66 @@ package de.oneos.cqrs.readmodels.amqp
 
 import com.rabbitmq.client.AMQP
 import com.rabbitmq.client.Channel
+import com.rabbitmq.client.Connection
+import com.rabbitmq.client.ConnectionFactory
 import de.oneos.cqrs.readmodels.EventFilter
 import de.oneos.cqrs.readmodels.EventProcessor
 import de.oneos.cqrs.readmodels.EventSupplier
 import de.oneos.cqrs.readmodels.MapEventFilter
+import domain.events.Event
+import domain.events.EventEnvelope
+import framework.EventPublisher
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 
 import static org.hamcrest.Matchers.equalTo
 import static org.junit.Assert.assertThat
+import static org.mockito.Matchers.anyObject
 import static org.mockito.Mockito.*
 
 class AMQPEventSupplierTest {
 
+    static final String APPLICATION_NAME = 'APPLICATION NAME'
+    static final String BOUNDED_CONTEXT_NAME = 'BOUNDED CONTEXT NAME'
+    static final String AGGREGATE_NAME = 'AGGREGATE NAME'
+    static final String EVENT_NAME = 'EVENT NAME'
+
     Channel channel = mock(Channel)
 
-    EventFilter eventFilter = mock(EventFilter)
+    EventFilter unconstrainedEventFilter = mock(EventFilter)
     EventProcessor eventProcessor = mock(EventProcessor)
 
     AMQP.Queue.DeclareOk queueDeclareOk
+    Connection connection
+
+    EventPublisher amqpEventPublisher
+    EventSupplier amqpEventSupplier
+
+    def generatedAggregateId = UUID.randomUUID()
+    def businessEventHappened = new BusinessEventHappened()
+
+    EventEnvelope boxedBusinessEvent = new EventEnvelope(APPLICATION_NAME, BOUNDED_CONTEXT_NAME, AGGREGATE_NAME, generatedAggregateId, businessEventHappened)
+
 
     @Before
     void setUp() {
         queueDeclareOk = new AMQP.Queue.DeclareOk.Builder().queue('GENERATED QUEUE NAME').build()
 
         when(channel.queueDeclare()).thenReturn(queueDeclareOk)
+
+        when(unconstrainedEventFilter.withConstrainedValues(anyObject(), anyObject())).thenReturn('*.*.*.*')
+
+        def connectionFactory = new ConnectionFactory()
+        connectionFactory.clientProperties = AMQPConstants.DEFAULT_AMQP_CLIENT_PROPERTIES
+        connection = connectionFactory.newConnection()
+
+        amqpEventPublisher = new AMQPEventPublisher(connection)
     }
 
 
     @Test
     void should_create_a_new_queue() {
-        EventSupplier amqpEventSupplier = new AMQPEventSupplier(channel: channel)
+        amqpEventSupplier = new AMQPEventSupplier(channel)
 
         verify(channel).queueDeclare()
         assertThat amqpEventSupplier.queueName, equalTo(queueDeclareOk.queue)
@@ -42,12 +70,17 @@ class AMQPEventSupplierTest {
     @Test
     void should_join_constrained_event_coordinates() {
         EventFilter eventFilter = new MapEventFilter(
-            applicationName: 'APPLICATION NAME',
-            boundedContextName: 'BOUNDED CONTEXT NAME',
-            aggregateName: 'AGGREGATE NAME',
-            eventName: 'EVENT NAME'
+            applicationName: APPLICATION_NAME,
+            boundedContextName: BOUNDED_CONTEXT_NAME,
+            aggregateName: AGGREGATE_NAME,
+            eventName: EVENT_NAME
         )
-        assertThat AMQPEventSupplier.routingKey(eventFilter), equalTo("${eventFilter.applicationName}.${eventFilter.boundedContextName}.${eventFilter.aggregateName}.${eventFilter.eventName}".toString())
+
+        assertThat AMQPEventSupplier.routingKey(eventFilter), equalTo(joinEventCoordinates(eventFilter, '.'))
+    }
+
+    private joinEventCoordinates(EventFilter eventFilter, String separator) {
+        ['applicationName', 'boundedContextName', 'aggregateName', 'eventName'].collect { eventFilter[it] }.join(separator)
     }
 
     @Test
@@ -59,16 +92,53 @@ class AMQPEventSupplierTest {
 
     @Test
     void should_create_a_binding_for_each_registered_event_filter() {
-        EventSupplier amqpEventSupplier = new AMQPEventSupplier(channel: channel)
+        amqpEventSupplier = new AMQPEventSupplier(channel)
 
-        amqpEventSupplier.subscribeTo(eventFilter, eventProcessor)
+        amqpEventSupplier.subscribeTo(unconstrainedEventFilter, eventProcessor)
 
-        verify(channel).queueBind(amqpEventSupplier.queueName, AMQPConstants.EVENT_EXCHANGE_NAME, AMQPEventSupplier.routingKey(eventFilter))
+        verify(channel).queueBind(amqpEventSupplier.queueName, AMQPConstants.EVENT_EXCHANGE_NAME, AMQPEventSupplier.routingKey(unconstrainedEventFilter))
     }
 
-    @Ignore
     @Test
     void should_pass_events_to_the_EventProcessor() {
+        amqpEventSupplier = new AMQPEventSupplier(connection)
+        amqpEventSupplier.subscribeTo(unconstrainedEventFilter, eventProcessor)
 
+        amqpEventPublisher.publish(boxedBusinessEvent)
+
+        // Because AMQP works inherently asynchronously we have to wait
+        sleep(50)
+
+        verify(eventProcessor).process(mapMatching(boxedBusinessEvent))
+    }
+
+    private mapMatching(EventEnvelope eventEnvelope) {
+        new Object() {
+            @Override
+            String toString() {
+                """{\
+applicationName == ${eventEnvelope.applicationName} && \
+boundedContextName == ${eventEnvelope.boundedContextName} && \
+aggregateName == ${eventEnvelope.aggregateName} && \
+aggregateId == ${eventEnvelope.aggregateId} && \
+eventName == ${eventEnvelope.eventName}\
+}"""
+            }
+
+            @Override
+            boolean equals(Object that) {
+                that.applicationName == eventEnvelope.applicationName &&
+                that.boundedContextName == eventEnvelope.boundedContextName &&
+                that.aggregateName == eventEnvelope.aggregateName &&
+                that.aggregateId == eventEnvelope.aggregateId.toString() &&
+                that.eventName == eventEnvelope.eventName
+            }
+        }
+    }
+
+    static class BusinessEventHappened extends Event {
+        String name = 'Business event happened'
+
+        Object applyTo(aggregate) { return null }
     }
 }
