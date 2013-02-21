f99edaae77ae2ed3e15fb6b47d49496eadbc31cd
Andreas Simon <a.simon@one-os.de>
2013-02-21 14:18:38 +0100

    Added generic types to EventStore

--- src/main/groovy/de/oneos/cqrs/eventstore/EventStore.groovy
+++ src/main/groovy/de/oneos/cqrs/eventstore/EventStore.groovy
@@ -6,7 +6,7 @@ import infrastructure.persistence.UnknownAggregate
 public interface EventStore {
 
     def save(EventEnvelope eventEnvelope)
-    def getAggregate(String applicationName, String boundedContextName, String aggregateName, Class aggregateClass, UUID aggregateId, String eventPackageName) throws UnknownAggregate
+    public <A> A getAggregate(String applicationName, String boundedContextName, String aggregateName, Class<A> aggregateClass, UUID aggregateId, String eventPackageName) throws UnknownAggregate
     List getEventsFor(String applicationName, String boundedContextName, String aggregateName, UUID aggregateId, String eventPackageName) throws UnknownAggregate
 
 }
