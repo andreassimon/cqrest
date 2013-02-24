3fcc20c7b866abf6ad13551c3b439cd6d08348b3
Andreas Simon <a.simon@one-os.de>
2013-02-24 14:01:13 +0100

    Let SpringJdbcEventStore create its events table

--- src/main/groovy/de/oneos/eventstore/springjdbc/SpringJdbcEventStore.groovy
+++ src/main/groovy/de/oneos/eventstore/springjdbc/SpringJdbcEventStore.groovy
@@ -1,9 +1,35 @@
 package de.oneos.eventstore.springjdbc
 
-import de.oneos.eventstore.EventStore
-import de.oneos.eventstore.UnitOfWork
+import de.oneos.eventstore.*
+
+import org.springframework.jdbc.core.JdbcOperations
+
 
 class SpringJdbcEventStore implements EventStore {
+
+    static String TABLE_NAME = 'events'
+
+
+    JdbcOperations jdbcTemplate
+
+    void createTable() {
+        jdbcTemplate.execute("""\
+CREATE TABLE ${TABLE_NAME} (
+    application_name     VARCHAR(255),
+    bounded_context_name VARCHAR(255),
+    aggregate_name       VARCHAR(255),
+    aggregate_id         UUID,
+    sequence_number      INTEGER,
+    event_name           VARCHAR(255),
+    attributes           TEXT,
+    timestamp            TIMESTAMP
+);\
+""")
+
+        ['application_name', 'bounded_context_name', 'aggregate_name', 'aggregate_id', 'event_name', 'timestamp'].each { columnName ->
+            jdbcTemplate.execute("CREATE INDEX idx_events_${columnName} ON events (${columnName});")
+        }
+    }
 
     @Override
     UnitOfWork createUnitOfWork() {
--- src/test/groovy/de/oneos/eventstore/springjdbc/SpringJdbcEventStoreTest.groovy
+++ src/test/groovy/de/oneos/eventstore/springjdbc/SpringJdbcEventStoreTest.groovy
@@ -1,15 +1,39 @@
 package de.oneos.eventstore.springjdbc
 
-import de.oneos.eventstore.*
 import org.junit.*
+import static org.junit.Assert.*
+import static org.mockito.Mockito.*
+import static org.mockito.Matchers.*
+import static org.hamcrest.Matchers.*
+
+import org.springframework.jdbc.core.*
+
+import de.oneos.eventstore.*
+
 
 class SpringJdbcEventStoreTest extends EventStore_ContractTest {
 
     EventStore eventStore
+    JdbcOperations mockJdbcTemplate = mock(JdbcOperations)
+
 
     @Before
     void setUp() {
         eventStore = new SpringJdbcEventStore()
+    }
+
+    @Test
+    void should_have_a_defined_TABLE_NAME() {
+        assertThat SpringJdbcEventStore.TABLE_NAME, equalTo('events')
+    }
+
+    @Test
+    void createTable__should_create_the_events_table() {
+        eventStore.jdbcTemplate = mockJdbcTemplate
+
+        eventStore.createTable()
+
+        verify(mockJdbcTemplate).execute(contains("CREATE TABLE ${SpringJdbcEventStore.TABLE_NAME}"))
     }
 
 }
