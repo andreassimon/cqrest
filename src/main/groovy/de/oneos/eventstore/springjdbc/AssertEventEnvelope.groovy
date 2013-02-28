12d9f889170970fbc1bbe85a8c43660a6a3756af
Andreas Simon <a.simon@one-os.de>
2013-02-28 16:45:34 +0100

    Added unit tests for AssertEventEnvelope

--- src/main/groovy/de/oneos/eventstore/springjdbc/AssertEventEnvelope.groovy
+++ src/main/groovy/de/oneos/eventstore/springjdbc/AssertEventEnvelope.groovy
@@ -2,15 +2,15 @@ package de.oneos.eventstore.springjdbc
 
 class AssertEventEnvelope {
 
-    static notEmpty(bean, String propertyName) {
-        AssertEventEnvelope.notNull(bean, propertyName)
-        if(bean[propertyName].empty) {
+    static void notEmpty(envelope, String propertyName) {
+        AssertEventEnvelope.notNull(envelope, propertyName)
+        if(envelope[propertyName].empty) {
             throw new IllegalArgumentException("The envelope's $propertyName must not be empty")
         }
     }
 
-    static notNull(bean, String propertyName) {
-        if (bean[propertyName] == null) {
+    static void notNull(envelope, String propertyName) {
+        if (envelope[propertyName] == null) {
             throw new IllegalArgumentException("The envelope's $propertyName must not be null")
         }
     }
