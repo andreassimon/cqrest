e74c9a5e109cf1f47106e7a4e8e0b152fa23ac3c
Andreas Simon <a.simon@one-os.de>
2013-04-07 18:37:55 +0200

    Added getSerializableForm() to interface Event

--- eventsourcing/src/main/groovy/de/oneos/eventsourcing/Event.groovy
+++ eventsourcing/src/main/groovy/de/oneos/eventsourcing/Event.groovy
@@ -3,6 +3,7 @@ package de.oneos.eventsourcing
 public interface Event<AT> {
 
     public String getEventName()
+    public def getSerializableForm()
     public <C extends Collection> C serializedProperties()
     public void applyTo(AT aggregate)
 
