7d9254c40544903ac84899cd6916573ce7ab6595
Andreas Simon <a.simon@quagilis.de>
2014-01-27 23:58:43 +0100

    CLOSED #16: Implemented EventProspectDiscoverer

---
+++ eventsourcing/src/main/groovy/de/oneos/eventsourcing/EventProspectDiscoverer.groovy
@@ -0,0 +1,33 @@
+package de.oneos.eventsourcing
+
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+import org.springframework.beans.BeansException
+
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+
+
+class EventProspectDiscoverer implements ApplicationContextAware {
+    public static Log log = LogFactory.getLog(this)
+
+
+    EventSupplier eventSupplier
+
+
+    EventProspectDiscoverer(EventSupplier eventSupplier) {
+        this.eventSupplier = eventSupplier
+    }
+
+
+    @Override
+    void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        applicationContext.getBeansWithAnnotation(EventProspect).each {
+            if(log.isDebugEnabled()) {
+                log.debug "Subscribing `$it.key` to `$eventSupplier`"
+            }
+            eventSupplier.subscribeTo(it.value)
+        }
+    }
+
+}
