9fde5c8f2705662ebd9b3fe3682e8d0c0d56a473
Andreas Simon <a.simon@quagilis.de>
2014-04-02 16:04:11 +0200

    #22 Added EventConsumerAdapter

---
+++ eventstore-inmemory/src/main/groovy/de/oneos/eventstore/inmemory/EventConsumerAdapter.groovy
@@ -0,0 +1,31 @@
+package de.oneos.eventstore.inmemory
+
+import de.oneos.eventsourcing.EventConsumer
+import de.oneos.eventsourcing.EventEnvelope
+
+
+class EventConsumerAdapter implements org.cqrest.reactive.Observer<EventEnvelope> {
+
+    final EventConsumer wrappee
+
+    EventConsumerAdapter(EventConsumer wrappee) {
+        assert wrappee
+        this.wrappee = wrappee
+    }
+
+    @Override
+    void onNext(EventEnvelope envelope) {
+        wrappee.process(envelope)
+    }
+
+    @Override
+    void onCompleted() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    void onError(Throwable e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+}
---
+++ eventstore-inmemory/src/test/groovy/de/oneos/eventstore/inmemory/EventConsumerAdapterTest.groovy
@@ -0,0 +1,40 @@
+package de.oneos.eventstore.inmemory;
+
+import de.oneos.eventsourcing.EventConsumer;
+import de.oneos.eventsourcing.EventEnvelope
+import de.oneos.eventstore.EventStore_ContractTest;
+import org.cqrest.reactive.Observer;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.verify;
+
+
+public class EventConsumerAdapterTest {
+
+    @Test
+    public void onNext_should_pass_the_EventEnvelope_to_the_wrapped_EventConsumer() throws Exception {
+        EventConsumer mockEventConsumer = mock(EventConsumer)
+
+        Observer<EventEnvelope> adapter = new EventConsumerAdapter(mockEventConsumer)
+        adapter.onNext(AnEventEnvelope.build())
+
+        verify(mockEventConsumer).process(AnEventEnvelope.build())
+    }
+
+}
+
+class AnEventEnvelope {
+    public static final String APPLICATION_NAME = 'APPLICATION'
+    public static final String BOUNDED_CONTEXT_NAME = 'BOUNDED CONTEXT'
+    public static UUID ORDER_ID = UUID.fromString('836ed0d1-e87f-4d70-80f3-7aa44d00ed5d')
+    public static UUID ANOTHER_ORDER_ID = UUID.fromString('92bc8efe-0f5e-42f7-8dd6-3029d2d1a4eb')
+    public static UUID CUSTOMER_ID = UUID.fromString('3faeb1ad-6d46-458e-9b77-ee3a6e0ff3ce')
+    public static UUID NO_CORRELATION_ID = null
+    public static String USER_UNKNOWN = null
+
+    static EventEnvelope build() {
+        new EventEnvelope(APPLICATION_NAME, BOUNDED_CONTEXT_NAME, EventStore_ContractTest.Order.aggregateName, ANOTHER_ORDER_ID, EventStore_ContractTest.orderLineWasRemoved(), NO_CORRELATION_ID, USER_UNKNOWN)
+    }
+
+}
