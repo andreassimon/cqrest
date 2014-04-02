70d1f77a5cf6d663b9840f4df0995d1169f4c260
Andreas Simon <a.simon@quagilis.de>
2014-04-02 16:15:52 +0200

    #22 Extracted AnEventEnvelope to separate source file

--- eventstore-inmemory/src/test/groovy/de/oneos/eventstore/inmemory/EventConsumerAdapterTest.groovy
+++ eventstore-inmemory/src/test/groovy/de/oneos/eventstore/inmemory/EventConsumerAdapterTest.groovy
@@ -1,13 +1,13 @@
 package de.oneos.eventstore.inmemory;
 
-import de.oneos.eventsourcing.EventConsumer;
-import de.oneos.eventsourcing.EventEnvelope
-import de.oneos.eventstore.EventStore_ContractTest;
-import org.cqrest.reactive.Observer;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock
 import static org.mockito.Mockito.verify;
+
+import de.oneos.eventsourcing.EventConsumer;
+import de.oneos.eventsourcing.EventEnvelope
+import org.cqrest.reactive.Observer;
 
 
 public class EventConsumerAdapterTest {
@@ -24,17 +24,3 @@ public class EventConsumerAdapterTest {
 
 }
 
-class AnEventEnvelope {
-    public static final String APPLICATION_NAME = 'APPLICATION'
-    public static final String BOUNDED_CONTEXT_NAME = 'BOUNDED CONTEXT'
-    public static UUID ORDER_ID = UUID.fromString('836ed0d1-e87f-4d70-80f3-7aa44d00ed5d')
-    public static UUID ANOTHER_ORDER_ID = UUID.fromString('92bc8efe-0f5e-42f7-8dd6-3029d2d1a4eb')
-    public static UUID CUSTOMER_ID = UUID.fromString('3faeb1ad-6d46-458e-9b77-ee3a6e0ff3ce')
-    public static UUID NO_CORRELATION_ID = null
-    public static String USER_UNKNOWN = null
-
-    static EventEnvelope build() {
-        new EventEnvelope(APPLICATION_NAME, BOUNDED_CONTEXT_NAME, EventStore_ContractTest.Order.aggregateName, ANOTHER_ORDER_ID, EventStore_ContractTest.orderLineWasRemoved(), NO_CORRELATION_ID, USER_UNKNOWN)
-    }
-
-}
