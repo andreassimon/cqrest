4daaf20c885e1bad8a4788f04e929a8dd60bbc3e
Andreas Simon <a.simon@quagilis.de>
2014-04-03 21:40:02 +0200

    Added SpringJdbcEventStore_as_EventStream_Test as contract test for EventStream

---
+++ eventstore-springjdbc/src/test/groovy/de/oneos/eventstore/springjdbc/SpringJdbcEventStore_as_EventStream_Test.groovy
@@ -0,0 +1,8 @@
+package de.oneos.eventstore.springjdbc
+
+import org.cqrest.eventstore.EventStream_ContractTest
+
+
+class SpringJdbcEventStore_as_EventStream_Test extends EventStream_ContractTest {
+
+}
